af16d0e3cef35c951f104d181e8d714fe9bcc7e5
s-light <git@s-light.eu>
2025-05-24 22:56:40 +0200

    fix formatting (VSCodium autoformat)

--- src/SimpleRFID.cpp
+++ src/SimpleRFID.cpp
@@ -3,43 +3,39 @@
     Released into the public domain.
 */
 
+#include "SimpleRFID.h"
 #include "Arduino.h"
 #include <SoftwareSerial.h>
-#include "SimpleRFID.h"
 
-SimpleRFID::SimpleRFID(uint8_t rx_pin, uint8_t tx_pin)
-{
+SimpleRFID::SimpleRFID(uint8_t rx_pin, uint8_t tx_pin) {
     _simpleRFID_SoftSerial = new SoftwareSerial(rx_pin, tx_pin);
-    _simpleRFID_SoftSerial->begin(9600); //The RFID readers baudrate is fixed at 9600
+    // The RFID readers baudrate is fixed at 9600
+    _simpleRFID_SoftSerial->begin(9600);
 }
 
-SimpleRFID::~SimpleRFID()
-{
-    delete _simpleRFID_SoftSerial;
-}
+SimpleRFID::~SimpleRFID() { delete _simpleRFID_SoftSerial; }
 
-bool SimpleRFID::available()
-{
-    return _simpleRFID_SoftSerial->available() > 0;
-}
+bool SimpleRFID::available() { return _simpleRFID_SoftSerial->available() > 0; }
 
-String SimpleRFID::readID()
-{
+String SimpleRFID::readID() {
     String result = "";
     unsigned char temp_char;
     int check_int;
-    do{
-        temp_char = (unsigned char) _simpleRFID_SoftSerial->read();
-        check_int = (int) temp_char;    //used to check, if temp_char is valid data
-        //Control-characters are rendered invalid data. Consult ASCII-table for further information.
-        if(check_int != 2 && check_int != 3 && check_int != 255)
-        {
-            result += (char) temp_char; //append valid character to output-string
+    do {
+        temp_char = (unsigned char)_simpleRFID_SoftSerial->read();
+        // used to check, if temp_char is valid data
+        check_int = (int)temp_char;
+        // Control-characters are rendered invalid data.
+        // Consult ASCII-table for further information.
+        if (check_int != 2 && check_int != 3 && check_int != 255) {
+            // append valid character to output-string
+            result += (char)temp_char;
         }
-    }while(check_int != 3); //the control-character "end of message" ends the reading process
-    while(_simpleRFID_SoftSerial->available())
-    {
-        char junk = _simpleRFID_SoftSerial->read();    //clear input buffer in case the reader read twice
+    } while (check_int != 3);
+    // the control-character "end of message" ends the reading process
+    while (_simpleRFID_SoftSerial->available()) {
+        // clear input buffer in case the reader read twice
+        char junk = _simpleRFID_SoftSerial->read();
     }
     return result;
 }
--- src/SimpleRFID.h
+++ src/SimpleRFID.h
@@ -1,6 +1,6 @@
 /*
-    SIMPLE_RFID.h - Library intended for easy use of the "Grove-125KHz RFID Reader v1.0".
-    Created by Roman Schulte and Luai Malek, April 4, 2019.
+    SIMPLE_RFID.h - Library intended for easy use of the "Grove-125KHz RFID
+   Reader v1.0". Created by Roman Schulte and Luai Malek, April 4, 2019.
     Released into the public domain.
 */
 
@@ -11,26 +11,25 @@
 #include "SoftwareSerial.h"
 
 /*
-    Abstraction handling the serial interface and data-type conversions internally.
-    Users only need to use the provided high-level functions.
+    Abstraction handling the serial interface and data-type conversions
+   internally. Users only need to use the provided high-level functions.
 */
-class SimpleRFID
-{
-    public:
-    //Constructor
+class SimpleRFID {
+public:
+    // Constructor
     SimpleRFID(uint8_t rx_pin, uint8_t tx_pin);
 
-    //Returns whether the reader is holding data, ready to be read
+    // Returns whether the reader is holding data, ready to be read
     bool available();
 
-    //Returns the ID of the currently read RFID-chip 
+    // Returns the ID of the currently read RFID-chip
     String readID();
 
-    //Destructor
+    // Destructor
     ~SimpleRFID();
 
-    private:
-    SoftwareSerial* _simpleRFID_SoftSerial;
+private:
+    SoftwareSerial *_simpleRFID_SoftSerial;
 };
 
 #endif
